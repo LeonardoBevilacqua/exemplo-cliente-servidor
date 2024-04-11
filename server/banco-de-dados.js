f46add4d21a4e295a04857cb929f2b645504b025
Leonardo Almeida Bevilacqua <lbevilacqua@lenovo.com>
2024-04-11 09:34:43 -0300

    Criando arquivo de tipos e banco de dados

---
+++ server/banco-de-dados.js
@@ -0,0 +1,77 @@
+import { Cachorro } from "./tipos";
+/**
+ * @type {Cachorro[]}
+ */
+export const cachorros = [
+  {
+    id: "1",
+    nome: "Samoieda",
+    caracteristicas: [
+      "O padrão da raça permite as cores branco puro, creme ou branco com biscoito. A altura dos machos é de 57 cm e das fêmeas 53 cm, com tolerância de 3 cm para mais ou para menos.",
+      'Apesar de domesticado, é um cão que cumpre exemplarmente suas funções enquanto animal de utilidade, é raro ver um Samoieda agressivo (estando nessa situação fora do padrão da raça), portanto não utilizado como cão de guarda e sim como companhia ou pastoreio, sendo conhecido como o "cão que ri". Se adapta muito bem com crianças e outros animais, sendo brincalhões até idade avançada. Destacaram-se em competições de exposição.',
+    ],
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/1/18/Samojed00.jpg",
+  },
+  {
+    id: "2",
+    nome: "Boiadeiro de Berna",
+    caracteristicas: [
+      "O pelo é longo e brilhante, liso ou ligeiramente ondulado. A cor de fundo deve ser preta, com marcas cor de fogo (castanho-avermelhado escuro) nas faces, por cima dos olhos, nos quatro membros e no peito e com marcas brancas:",
+      "marcas bem brancas na cabeça, claramente delimitadas e simétricas; lista que, em direcção à trufa, se alarga dos dois lados do chanfro para desenhar o branco no focinho;",
+      "em largura, a lista não deverá ir até às marcas fogo por cima dos olhos, e o branco no focinho não deverá ultrapassar a comissura dos lábios;",
+      "região branca ininterrupta e moderadamente larga no pescoço e peito.",
+      "procurado: pés e ponta da cauda brancos;",
+      "tolerado: pequena mancha branca na nuca e sob a cauda.",
+    ],
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Bernese_Mountain_DOg2.jpg/1920px-Bernese_Mountain_DOg2.jpg",
+  },
+  {
+    id: "3",
+    nome: "Golden retriever",
+    caracteristicas: [
+      'Algumas variações que existem entre a Golden Retrievers tipo britânico prevalente em toda a Europa e Austrália, e os de linhas americanas são refletidas no padrão da raça. O focinho do tipo britânico de cachorro é mais largo e mais curto, e sua testa é mais quadrada. Ele tem as pernas mais curtas, com um peito um pouco mais profundo, e cauda mais curta. Suas características tornam geralmente mais pesadas do que o de tipo americano. Os machos devem ter entre 56-61 cm na cernelha e as fêmeas ligeiramente mais curto, entre 51-56 cm. O peso, no entanto, não é especificado no padrão do Reino Unido. As chamadas padrão KC para a linha superior nivelada e traseiros em linha reta, sem a angulação ligeira traseira encontrados em linhas americanas. Os olhos do tipo europeu são notáveis por sua circularidade e escuridão em contraste com a composição triangular ou inclinada de suas contrapartes americanas. Um Golden Retriever de reprodução britânico pode ter uma cor da pelagem de qualquer tonalidade de dourado ou creme, porém, vermelho ou mogno não são cores permitidas. Originalmente creme não era uma cor aceitável no padrão do Reino Unido, no entanto, em 1936 o padrão foi revisado para incluir creme. Considerou-se essa exclusão foi um erro que o original "amarelo" retrievers do século XIX eram de cor mais clara que a norma então vigente permitido. Tal como acontece com as linhas norte-americano, branco é uma cor inaceitável no anel da mostra. O padrão britânico KC é usado em todos os países, com as excepções dos EUA e Canadá. Alguns criadores deste tipo na América pode importar os seus cães para melhorar o temperamento e a saúde observou nessas linhagens.',
+      "Um Golden Americano é mais esguio e menos encorpado do que o tipo britânico. Um macho deve ficar entre 56-61 cm de altura nos ombros, e as fêmeas devem ser 51-56 cm. A pelagem é densa e repelente de água, em vários tons de ouro e brilhante, com franjas moderadas. A movimentação deve ser livre, suave, poderoso e bem coordenado.",
+    ],
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/f/f0/Goldensondika.jpg",
+  },
+  {
+    id: "4",
+    nome: "Shih-tzu",
+    caracteristicas: [
+      "Fisicamente possuem olhos grandes e escuros, pelagem exuberante e podem chegar a atingir os 28 cm na cernelha e pesarem de 4,5 a 8,6 kg. É considerado um típico animal de companhia, apesar de menos ativo que outros de mesmo porte. No livro A Inteligência dos Cães, de Stanley Coren, ocupa a 70ª posição das 79. Apesar da baixa classificação, o índice desta publicação considera como mais inteligentes as raças que obedecem mais facilmente a comandos e respondem mais rapidamente ao adestramento, considerado um ponto fraco do shih-tzu, pois é um cão que manifesta comportamento bastante dominante. Em aspectos cognitivos no entanto, a raça apresenta uma inteligência acima da média, sendo os exemplares quietos e bastante observadores, curiosos, exploradores do ambiente e buscam grande empatia emocional com os donos.",
+      "No geral, o shih-tzu se adapta muito bem à companhia de crianças e outros animais de estimação. Além disso, ele é um cão ideal para a cidade e para o campo. Na cidade, esses são cães que se acostumam facilmente a barulhos e vivem bem em apartamentos - local mais apropriado. Já no campo, eles exigem muito mais cuidado, devem ser mantidos dentro de casa e tomar banho regularmente.",
+    ],
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Shih-Tzu.jpg",
+  },
+  {
+    id: "5",
+    nome: "Malamute-do-alasca",
+    caracteristicas: [
+      "Fisicamente, estes animais podem chegar a medir de 58 a 64 cm na cernelha e pesar entre 34 e 38 kg. A pelagem, densa e com subpelo, é considerada macia e rústica, embora não seja longa. Os malamutes-do-alasca são uma raça de grande porte usados como cães de trenó para viagens pessoais, transportar mercadorias, ajudar a mover objetos, ou trenó como lazer. No entanto a maioria é composta de animais de estimação da família ou cães de exposição. Malamutes são geralmente lentos em corridas de longa distância. Sua utilidade é bastante limitada para fazer viagens rápidas.",
+      "Seu temperamento é classificado como amistoso e afetivo, bem como companheiro, brincalhão e por vezes, maduro.",
+    ],
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/e/e6/Alaskan_Malamute_600.jpg",
+  },
+  {
+    id: "6",
+    nome: "Chow-chow",
+    caracteristicas: [
+      "O chow chow é conhecido pela sua aparência de “cão-leão” ou “cão-urso”.",
+      "Chow chow e shar-pei são as únicas raças de cães que têm a língua azul.",
+      "O chow chow ocupa atualmente o 76° lugar no ranking de inteligência canina, entre as 79 colocações, de acordo com o livro de Stanley Coren A Inteligência dos Cães, de 1995.",
+      "São cães territorialistas e atuam muito bem como cães de guarda silenciosos. Por, normalmente, possuírem temperamento dominante, esses cães podem causar brigas com outros cães, ou mesmo desafiar seus donos, sendo indicados para proprietários experientes que adotem o adestramento desde cedo. Porém, não é uma raça considerada agressiva.",
+      "Os machos podem ter entre 48 à 56 cm de altura na cernelha, e as fêmeas entre 46 e 51 cm na cernelha. Eles podem se apresentar em 5 cores de pelagem, todas em forma unicolor: preto, vermelho, azul (cinza), fulvo e creme; frequentemente com nuanças, mas sem manchas. Foram utilizados como cães de rinha, de guarda, de caça e como cães de trenó.",
+    ],
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Shiva%2C_aug-05-5.jpg/300px-Shiva%2C_aug-05-5.jpg",
+  },
+  {
+    id: "7",
+    nome: "Husky siberiano",
+    caracteristicas: [
+      "A pelagem do Husky possui subpelo e é bastante variada em questão de cor, desde o completamente branco ao preto-e-branco, chocolate-e-branco, cinza-e-branco, além de outras variações. Quanto a cor dos olhos, o padrão oficial da raça estabelece:",
+      "A cor dos olhos podem ser castanhos ou azuis; aceitam-se os olhos de cores diferentes [heterocromia] ou um olho particolorido [heterocromia sectorial]. A expressão é penetrante, porém amigável, interessada e até um pouco maliciosa.",
+      "Os machos podem possuir até 60 cm de altura na cernelha e pesar cerca de 27 Kg; as fêmeas podem possuir até 56 cm de altura e pesar cerca de 25 Kg. São cães atléticos e velozes e são mais recomendados para trenós em viagens de longa distância com cargas mais leves, ao contrário do seu parente de porte maior, o Malamute-do-alasca.",
+    ],
+    imagem: "https://upload.wikimedia.org/wikipedia/commons/a/a3/Black-Magic-Big-Boy.jpg",
+  },
+];
---
+++ server/tipos.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef {Object} ListaDeCachorros
+ * @property {string} id
+ * @property {string} nome
+ */
+
+/**
+ * @typedef {Object} Cachorro
+ * @property {string} id
+ * @property {string} nome
+ * @property {string} imagem
+ * @property {string[]} caracteristicas
+ */
+
+const Tipos = {};
+
+export default Tipos;
